ab9da4acc7e0f90f4b185eb869128abc8c5515b7
Adrian082818 <mohabiradrian@gmail.com>
2020-03-31 18:07:35 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,20 +1,74 @@
-setTimeout(start);
-let i = 0;
-let num = document.getElementById('counter');
-function start() {
-setInterval(increase, 1000);
-function increase() {
-    if (i < 10000000) {
-        i++;
-        num.innerText = i;
-    }
-}
-}
+// document.addEventListener("DOMcontentloaded", () => {
+    // document.addEventListener("DOMcontentloaded", function() {
+    let counter = document.querySelector('#counter');
 
-function addComment() {
-    let comment = document.getElementById('comment-input').value
-    let list = document.getElementById('list')
-    let newComment = document.createElement('p')
-    newComment.innerText = comment 
-    list.appendChild(newComment)
-}
+        let timer = setInterval(function() {
+            counter.innerText++;
+        }, 1000);
+       
+    let pauseButton = document.getElementById("pause")
+    let plusButton = document.getElementById("plus")
+    let heartButton = document.getElementById("heart")
+    let minusButton = document.getElementById("minus")
+    let likes = document.querySelector(".likes")
+
+    let comments = document.querySelector("#list")
+    let submitButton = document.querySelector("#submit")
+
+        
+
+        
+     minusButton.addEventListener('click', function(e) {
+        counter.innerText--;
+     });
+
+     plusButton.addEventListener('click', function(e) {
+         counter.innerText++;
+     });
+        
+        // let clicks = 0;
+        // function click() {
+            //     clicks += 1;
+            //     document.getElementById('heart').innerHTML = clicks;
+            // };
+            
+            pauseButton.addEventListener('click', function(e) {
+                if (pauseButton.innerText === "pause") {
+                    pauseButton.innerText = "resume"
+                    clearInterval(timer)
+                    
+                    plusButton.disabled = true;
+                    minusButton.disabled = true;
+                    heartButton.disabled = true;
+                    submitButton.disabled = true;
+                }
+                else {
+                    timer = setInterval(function() {
+                        counter.innerText++;
+                    }, 1000);
+                    pauseButton.innerText = "pause";
+                    
+                    plusButton.disabled = false;
+                    minusButton.disabled = false;
+                    heartButton.disabled = false;
+                    submitButton.disabled = false;
+                }
+                
+            });
+            
+            heartButton.addEventListener('click', function(e) {
+                let obj = document.getElementById(`${counter.innerText}`);
+                obj ? obj.children[0].innerText++:
+                likes.innerHTML += `<li id=${counter.innerText}>${counter.innerText} is liked <span id=${counter.innerText}>1</span> times.</li>`
+                
+            });
+            
+                
+                submitButton.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    let comment = document.querySelector('#comment-form > input[type=text]').value
+                    comments.innerHTML += `<li>${comment}</li>`
+                    document.querySelector('#comment-form').reset() 
+                });
+            // });              
+// });
